28c1b9f3cbcfd98fc52627aeda4d963e16741931
Knremo <ficnomre@outlook.com>
2019-02-28 15:38:05 +0800

    2.1

--- exercises/2.5/demo.cpp
+++ exercises/2.5/demo.cpp
@@ -3,7 +3,69 @@
 
 int main()
 {
-    unsigned s = 10;
-    std::cout << typeid(L"a").name() << std::endl;
-    std::cout << sizeof(long) << std::endl;
+    std::cout << "'a' --> " << typeid('a').name() << "(char)"
+              << " " << sizeof('a') << " bytes." << std::endl;
+    std::cout << "L'a' --> " << typeid(L'a').name() << "(wchar_t)"
+              << " " << sizeof(L'a') << " bytes." << std::endl;
+    std::cout << "\"a\" --> " << typeid("a").name() << "(char array 2x1)"
+              << " " << sizeof("a") << " bytes." << std::endl;
+    std::cout << "L\"a\" --> " << typeid(L"a").name() << "(wchar_t array 2x1)"
+              << " " << sizeof(L"a") << " bytes." << std::endl;
+
+    std::cout << "u'a' --> " << typeid(u'a').name() << "(char16_t)"
+              << " " << sizeof(u'a') << " bytes." << std::endl;
+    std::cout << "u\"a\" --> " << typeid(u"a").name() << "(char16_t array 2x1)"
+              << " " << sizeof(u"a") << " bytes." << std::endl;
+    std::cout << "u\"想你\" --> " << typeid(u"想你").name() << "(char16_t array 3x1)"
+              << " " << sizeof(u"想你") << " bytes." << std::endl;
+
+    std::cout << "U'a' --> " << typeid(U'a').name() << "(char32_t)"
+              << " " << sizeof(U'a') << " bytes." << std::endl;
+    std::cout << "U\"a\" --> " << typeid(U"a").name() << "(char32_t array 2x1)"
+              << " " << sizeof(U"a") << " bytes." << std::endl;
+    std::cout << "U\"煎熬\" --> " << typeid(U"煎熬").name() << "(char32_t array 3x1)"
+              << " " << sizeof(U"煎熬") << " bytes." << std::endl;
+
+    std::cout << "u8\"a\" --> " << typeid(u8"a").name() << "(char array 2x1)"
+              << " " << sizeof(u8"a") << " bytes." << std::endl;
+    std::cout << "u8\"我的\" --> " << typeid(u8"我的").name() << "(char array 7x1)"
+              << " " << sizeof(u8"我的") << " bytes." << std::endl;
+
+    std::cout << "\"\u5929\u624d\" --> " << typeid("\u5929\u624d").name() << "(char array 7x1)"
+              << " " << sizeof("\u5929\u624d") << " bytes." << std::endl;
+
+    std::cout << "10 --> " << typeid(10).name() << "(int)"
+              << " " << sizeof(10) << " bytes." << std::endl;
+    std::cout << "10u --> " << typeid(10u).name() << "(unsigned int)"
+              << " " << sizeof(10u) << " bytes." << std::endl;
+    std::cout << "10uL --> " << typeid(10uL).name() << "(unsigned long)"
+              << " " << sizeof(10uL) << " bytes." << std::endl;
+    std::cout << "012 --> " << typeid(012).name() << "(int)"
+              << " " << sizeof(012) << " bytes." << std::endl;
+    std::cout << "0xC --> " << typeid(0xC).name() << "(int)"
+              << " " << sizeof(0xC) << " bytes." << std::endl;
+
+    long l = 12345;
+    long long ll = 12345;
+    short s = 10;
+    std::cout << "long --> " << typeid(l).name() << "(long)"
+              << " " << sizeof(l) << " bytes." << std::endl;
+    std::cout << "long long --> " << typeid(ll).name() << "(long long)"
+              << " " << sizeof(ll) << " bytes." << std::endl;
+    std::cout << "short --> " << typeid(s).name() << "(short)"
+              << " " << sizeof(s) << " bytes." << std::endl;
+
+    std::cout << "3.14 --> " << typeid(3.14).name() << "(double)"
+              << " " << sizeof(3.14) << " bytes." << std::endl;
+    std::cout << "3.14F --> " << typeid(3.14F).name() << "(float)"
+              << " " << sizeof(3.14F) << " bytes." << std::endl;
+    std::cout << "3.14L --> " << typeid(3.14L).name() << "(long double)"
+              << " " << sizeof(3.14L) << " bytes." << std::endl;
+    
+    char i[3][4];
+    std::cout << typeid(i).name() << std::endl;
+
+    std::cout << "\145?\012E" << std::endl; //2.7
+
+    std::cout << "2\tM\n133\tWDADA" << std::endl; //2.8
 }
--- notes/unit2.md
+++ notes/unit2.md
@@ -24,3 +24,65 @@
 `char`由编译器决定是`signed char` or `unsigned char`.
 
 ## 2.1.2 类型转换
+
+* `bool b = 42` //初始值为0结果为false,否则为true
+
+* `int i = 3.14`//i=3,仅保留浮点数中小数点之前的部分
+
+* `unsigned char c = -1` //c=255
+
+* `signed char c2 = 256` //undefined
+
+### 含有无符号类型的表达式
+算术表达式中既有无符号数又有int值时，int值会转换成无符号数.
+
+从无符号数中减去一个值时，不管这个值是不是无符号数，须确保结果不为负.
+
+## 2.1.3 字面值常量
+### 整型和浮点型字面值
+`20 024 0x14` 十进制类型是int,long,long long中尺寸最小的；八进制十六进制是int,unsigned int,unsigned long,unsigned long long.
+
+`3.14 3.14E0 0. 0e0 .001` double.
+
+### 字符和字符串字面值
+`'a'`
+
+`"Hello World!"` 字符数组，结尾有`'\0'`.
+
+```
+std::cout << "c1"
+             "c2" << std::endl;
+```
+
+### 转义序列
+```
+\n //换行符
+\t //tab
+\a //alert
+\v //纵向制表符
+\b //backspace
+\"
+\\
+\?
+\'
+\r //回车符
+\f //进纸符
+```
+
+`'\115' or '\x4d'` = `'M'`
+
+### 指定字面值类型
+|前缀|类型|
+|---|---|
+|u|char16_t|
+|U|char32_t|
+|L|wchar_t|
+|u8|char|
+
+|后缀|类型|
+|---|---|
+|u or U|unsigned|
+|l or L|long|
+|ll or LL|long long|
+|f or F|float|
+|l or L|long double|
