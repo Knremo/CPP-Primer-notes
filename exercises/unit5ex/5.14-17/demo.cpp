4b80ee473593f022fe62a413a24473a85f98e343
Knremo <ficnomre@outlook.com>
2019-07-05 00:26:55 +0800

    7.5

--- exercises/unit5ex/5.14-17/demo.cpp
+++ exercises/unit5ex/5.14-17/demo.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -27,9 +28,50 @@ void f1()
     cout << last << " " << last_cnt << endl;
 }
 
+//5.15
+void f2()
+{
+    for (int ix = 0;; ++ix)
+        if (ix != 10)
+            ;
+
+    int ix = 0;
+    for (; ix != 10; ++ix)
+        ;
+
+    for (int ix = 0; ix != 10; ++ix)
+        ;
+}
+
+//5.16
+
+//5.17
+void f3()
+{
+    vector<int> v1 = {0, 1, 1, 2, 5};
+    vector<int> v2 = {0, 2, 1};
+
+    auto v1beg = v1.begin(), v1end = v1.end();
+    auto v2beg = v2.begin(), v2end = v2.end();
+
+    while (v1beg != v1end && v2beg != v2end)
+    {
+        if (*v1beg != *v2beg)
+            break;
+        v1beg++, v2beg++;
+    }
+
+    if (v1beg != v1end && v2beg != v2end) cout << 0 << endl;
+    else
+    {
+        cout << 1 << endl;
+    }
+    
+}
+
 int main()
 {
-    f1();
+    f3();
 
     return 0;
 }
