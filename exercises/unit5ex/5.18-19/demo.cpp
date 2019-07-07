ac17be8d42d39a1db4676d527ed5ca7cb98490b2
Knremo <ficnomre@outlook.com>
2019-07-08 00:18:58 +0800

    7.8

--- exercises/unit5ex/5.18-19/demo.cpp
+++ exercises/unit5ex/5.18-19/demo.cpp
@@ -13,11 +13,30 @@ void f1()
             cout << "Sum is: " << v1 + v2 << endl;
     } while (cin);
 
+    // do {
+    //     ;
+    // } while (int ival = 1); //x
+
+    // do {
+    //     int ival = 1;
+    // } while (ival); //x , ival is undefined.
+}
+
+//5.19
+void f2()
+{
+    do
+    {
+        string s1, s2;
+        cout << "Please enter 2 strings:";
+        cin >> s1 >> s2;
+        cout << (s1.size() < s2.size() ? s1 : s2) << endl;
+    } while (cin);
 }
 
 int main()
 {
-    f1();
+    f2();
 
     return 0;
 }
