139e3678be62797a2302bd471a740e225b124046
knremo <ficnomre@outlook.com>
2019-10-07 21:11:49 +0800

    10.7

--- exercises/unit9ex/9.52/demo.cpp
+++ exercises/unit9ex/9.52/demo.cpp
@@ -7,5 +7,16 @@ using namespace std;
 int main()
 {
     string expression = "3 * (5+ ( 56 - 6) )/10";
-    stack<char> cstack;
+    stack<char, string> stk;
+
+    for (size_t i = expression.size(); i > 0; --i)
+    {
+        stk.push(expression[i-1]);
+    }
+    cout << stk.top() << endl;
+
+    while (!stk.empty())
+    {
+        
+    }
 }
