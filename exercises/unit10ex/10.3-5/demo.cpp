7ea983a35027cfe5bcd54be44227bac8538a5e9a
knremo <ficnomre@outlook.com>
2019-10-14 22:13:33 +0800

    10.14

---
+++ exercises/unit10ex/10.3-5/demo.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+#include <numeric>
+#include <iostream>
+
+using namespace std;
+
+//10.3
+void f1()
+{
+    vector<int> iv = {1,2,3,4,5,6};
+
+    cout << accumulate(iv.cbegin(), iv.cend(), 0) << endl;
+}
+
+// 10.4
+void f2()
+{
+    vector<double> iv = {1.0,2.5,3.0,4.5,5.5,6.5};
+    //cout << accumulate(iv.cbegin(), iv.cend(), 0) << endl; //小数部分会被舍掉 21
+    cout << accumulate(iv.cbegin(), iv.cend(), 0.0) << endl;
+}
+
+//10.5 两个都是也没啥问题
+void f3()
+{
+    vector<const char*> cv1 = {"s1", "s2"};
+    //vector<string> cv1 = {"s1", "s2"};
+    vector<const char*> cv2 = {"s1", "s2", "s3", "s4"};
+    
+    cout << equal(cv1.cbegin(), cv1.cend(), cv2.cbegin()) << endl;
+    //cout << *cv1.begin() << endl;
+}
+
+int main()
+{
+    f3();
+
+    const char* p = "abc";
+    //cout << *(p+3) << endl;
+    //cout << p[0] << endl;
+    cout << p << endl;
+    
+    return 0;
+}
--- notes/unit10.md
+++ notes/unit10.md
@@ -6,8 +6,19 @@ auto result = find(vec.cbegin(), vec.cend(), val);
 # 10.2 初识泛型算法
 ## 10.2.1 只读算法
 find,count,
+
+accumulate
 ```c++
+#include <numeric>
+
 int sum = accumulate(vec.cbegin(), vec.cend(), 0);
 // 求和（迭代器范围，和的初值）
 // int double long string
 ```
+
+equal
+```c++
+equal(roster1.cbegin(), roster1.end(), roster2.cbegin());
+// true or false
+```
+## 10.2.2 写容器元素的算法
